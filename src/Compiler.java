a9ac4f6db3dc1b9092ad6fa97d7a4d46bb091128
roudi <sajjad.roodi@gmail.com>
2022-12-09 19:31:21 +0330

    Add initial java classes

---
+++ src/Compiler.java
@@ -0,0 +1,34 @@
+import gen.ToorlaLexer;
+import gen.ToorlaListener;
+import gen.ToorlaParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
+public class Compiler {
+
+    private static final String SAMPLE_TEST_FILE_PATH = "sample/input.trl";
+
+    public static void main(String[] args) throws IOException {
+        ParseTree tree = buildTree();
+        ToorlaListener listener = new ProgramPrinter();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
+    }
+
+    private static ParseTree buildTree() throws IOException {
+        CharStream stream = CharStreams.fromFileName(SAMPLE_TEST_FILE_PATH);
+        ToorlaLexer lexer = new ToorlaLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        ToorlaParser parser = new ToorlaParser(tokens);
+        parser.setBuildParseTree(true);
+        return parser.program();
+    }
+
+}
---
+++ src/ProgramPrinter.java
@@ -0,0 +1,437 @@
+import gen.ToorlaListener;
+import gen.ToorlaParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public class ProgramPrinter implements ToorlaListener {
+    @Override
+    public void enterProgram(ToorlaParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void exitProgram(ToorlaParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void enterClassDeclaration(ToorlaParser.ClassDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitClassDeclaration(ToorlaParser.ClassDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterEntryClassDeclaration(ToorlaParser.EntryClassDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitEntryClassDeclaration(ToorlaParser.EntryClassDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterFieldDeclaration(ToorlaParser.FieldDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitFieldDeclaration(ToorlaParser.FieldDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterAccess_modifier(ToorlaParser.Access_modifierContext ctx) {
+
+    }
+
+    @Override
+    public void exitAccess_modifier(ToorlaParser.Access_modifierContext ctx) {
+
+    }
+
+    @Override
+    public void enterMethodDeclaration(ToorlaParser.MethodDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void exitMethodDeclaration(ToorlaParser.MethodDeclarationContext ctx) {
+
+    }
+
+    @Override
+    public void enterClosedStatement(ToorlaParser.ClosedStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitClosedStatement(ToorlaParser.ClosedStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterClosedConditional(ToorlaParser.ClosedConditionalContext ctx) {
+
+    }
+
+    @Override
+    public void exitClosedConditional(ToorlaParser.ClosedConditionalContext ctx) {
+
+    }
+
+    @Override
+    public void enterOpenConditional(ToorlaParser.OpenConditionalContext ctx) {
+
+    }
+
+    @Override
+    public void exitOpenConditional(ToorlaParser.OpenConditionalContext ctx) {
+
+    }
+
+    @Override
+    public void enterOpenStatement(ToorlaParser.OpenStatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitOpenStatement(ToorlaParser.OpenStatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatement(ToorlaParser.StatementContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatement(ToorlaParser.StatementContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementVarDef(ToorlaParser.StatementVarDefContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementVarDef(ToorlaParser.StatementVarDefContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementBlock(ToorlaParser.StatementBlockContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementBlock(ToorlaParser.StatementBlockContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementContinue(ToorlaParser.StatementContinueContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementContinue(ToorlaParser.StatementContinueContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementBreak(ToorlaParser.StatementBreakContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementBreak(ToorlaParser.StatementBreakContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementReturn(ToorlaParser.StatementReturnContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementReturn(ToorlaParser.StatementReturnContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementClosedLoop(ToorlaParser.StatementClosedLoopContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementClosedLoop(ToorlaParser.StatementClosedLoopContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementOpenLoop(ToorlaParser.StatementOpenLoopContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementOpenLoop(ToorlaParser.StatementOpenLoopContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementWrite(ToorlaParser.StatementWriteContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementWrite(ToorlaParser.StatementWriteContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementAssignment(ToorlaParser.StatementAssignmentContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementAssignment(ToorlaParser.StatementAssignmentContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementInc(ToorlaParser.StatementIncContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementInc(ToorlaParser.StatementIncContext ctx) {
+
+    }
+
+    @Override
+    public void enterStatementDec(ToorlaParser.StatementDecContext ctx) {
+
+    }
+
+    @Override
+    public void exitStatementDec(ToorlaParser.StatementDecContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpression(ToorlaParser.ExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpression(ToorlaParser.ExpressionContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionOr(ToorlaParser.ExpressionOrContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionOr(ToorlaParser.ExpressionOrContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionOrTemp(ToorlaParser.ExpressionOrTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionOrTemp(ToorlaParser.ExpressionOrTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionAnd(ToorlaParser.ExpressionAndContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionAnd(ToorlaParser.ExpressionAndContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionAndTemp(ToorlaParser.ExpressionAndTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionAndTemp(ToorlaParser.ExpressionAndTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionEq(ToorlaParser.ExpressionEqContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionEq(ToorlaParser.ExpressionEqContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionEqTemp(ToorlaParser.ExpressionEqTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionEqTemp(ToorlaParser.ExpressionEqTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionCmp(ToorlaParser.ExpressionCmpContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionCmp(ToorlaParser.ExpressionCmpContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionCmpTemp(ToorlaParser.ExpressionCmpTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionCmpTemp(ToorlaParser.ExpressionCmpTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionAdd(ToorlaParser.ExpressionAddContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionAdd(ToorlaParser.ExpressionAddContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionAddTemp(ToorlaParser.ExpressionAddTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionAddTemp(ToorlaParser.ExpressionAddTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionMultMod(ToorlaParser.ExpressionMultModContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionMultMod(ToorlaParser.ExpressionMultModContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionMultModTemp(ToorlaParser.ExpressionMultModTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionMultModTemp(ToorlaParser.ExpressionMultModTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionUnary(ToorlaParser.ExpressionUnaryContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionUnary(ToorlaParser.ExpressionUnaryContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionMethods(ToorlaParser.ExpressionMethodsContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionMethods(ToorlaParser.ExpressionMethodsContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionMethodsTemp(ToorlaParser.ExpressionMethodsTempContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionMethodsTemp(ToorlaParser.ExpressionMethodsTempContext ctx) {
+
+    }
+
+    @Override
+    public void enterExpressionOther(ToorlaParser.ExpressionOtherContext ctx) {
+
+    }
+
+    @Override
+    public void exitExpressionOther(ToorlaParser.ExpressionOtherContext ctx) {
+
+    }
+
+    @Override
+    public void enterToorlaType(ToorlaParser.ToorlaTypeContext ctx) {
+
+    }
+
+    @Override
+    public void exitToorlaType(ToorlaParser.ToorlaTypeContext ctx) {
+
+    }
+
+    @Override
+    public void enterSingleType(ToorlaParser.SingleTypeContext ctx) {
+
+    }
+
+    @Override
+    public void exitSingleType(ToorlaParser.SingleTypeContext ctx) {
+
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode terminalNode) {
+
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode errorNode) {
+
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
+
+    @Override
+    public void exitEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
+}
